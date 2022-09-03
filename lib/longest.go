b9e00b7e4cbd86c3584af619b5af66b10fd19098
Jim Wharton <jamesawharton@gmail.com>
2022-09-03 15:00:39 -0600

    chore: get test passing

--- lib/longest.go
+++ lib/longest.go
@@ -1,5 +1,27 @@
 package lib
 
+import (
+    "bytes"
+    "sort"
+)
+
 func LongestCommonPrefix(strs []string) string {
-    return "hi"
+    sort.Strings(strs)
+    pointer := 0
+    firstStr := strs[0]
+    lastStr := strs[len(strs) - 1]
+    var longestPrefix bytes.Buffer
+    longestPrefix.WriteString("")
+
+    for pointer <= len(firstStr) - 1 {
+        if (firstStr[pointer] != lastStr[pointer]) {
+            break
+        }
+
+
+        longestPrefix.WriteByte(firstStr[pointer]) 
+        pointer++
+    }
+
+    return longestPrefix.String()
 }
