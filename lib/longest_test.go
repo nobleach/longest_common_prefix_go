658949343f6c3f6da7f59f75858687b2ec8c23a0
Jim Wharton <jamesawharton@gmail.com>
2022-09-03 15:03:10 -0600

    test: get all tests passing

--- lib/longest_test.go
+++ lib/longest_test.go
@@ -12,3 +12,12 @@ func TestReturnsFl(t *testing.T) {
         t.Errorf("Expected fl, got %s", result)
     }
 }
+
+func TestReturnsEmpty(t *testing.T) {
+    strs := []string{"flower", "glow", "flight", "fly", "floor", "flu", "flag"}
+    result := LongestCommonPrefix(strs)
+
+    if result != "" {
+        t.Errorf("Expected '', got %s", result)
+    }
+}
